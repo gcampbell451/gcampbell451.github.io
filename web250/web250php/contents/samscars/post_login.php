f97c9edb182492bad9d4a9c7f52f9999cca3278e
gcampbell451 <gcampbell451@gmail.com>
2022-05-08 18:55:03 -0400

    Update post_login.php

--- web250/web250php/contents/samscars/post_login.php
+++ web250/web250php/contents/samscars/post_login.php
@@ -38,3 +38,16 @@ else
     header("location: ?p=login.php");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "styles/default.css" />
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
