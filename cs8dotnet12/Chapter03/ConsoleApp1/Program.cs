8690ec492e297467aaa5bf12b202b22a22afd4d0
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-03-17 17:45:59 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter03/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+#region Handling Exceptions
+/*
+WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+
+try
+{
+    int age = int.Parse(input!);
+    WriteLine($"You are {age} years old.");
+}
+catch (FormatException)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
+catch (OverflowException)
+{
+    WriteLine("Your age is a valid number format but it was too big or to small for Int32.");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing");
+*/
+#endregion
+
+#region Checking for overflow
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+}
+catch
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+
+#endregion
