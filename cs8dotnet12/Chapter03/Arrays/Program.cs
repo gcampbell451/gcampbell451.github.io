0498ab707488f85466846aad7105d10472efaea2
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-03-17 17:43:50 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter03/Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿#region Working with multi-dimensional arrays
+/*
+string[,] grid1 =
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+Console.WriteLine($"1st dimension, lower bound: {grid1.GetLowerBound(0)}");
+Console.WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
+Console.WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
+Console.WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+    {
+        Console.WriteLine($"Row {row}, Col {col}: {grid1[row, col]}");
+    }
+}
+*/
+
+#endregion
+
+#region List pattern matching with arrays
+int[] sequentialNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] oneTwoNumbers = { 1, 2 };
+int[] oneTwoTenNumbers = { 1, 2, 10 };
+int[] oneTwoThreeTenNumbers = { 1, 2, 3, 10};
+int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+int[] fibonacciNumbers = {  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+int[] emptyNumbers = { };
+int[] threeNumbers = { 9, 7, 5 };
+int[] sixNumbers = { 9, 7, 5, 4, 2, 10 };
+
+Console.WriteLine($"{nameof(sequentialNumbers)}: {CheckSwitch(sequentialNumbers)}");
+Console.WriteLine($"{nameof(oneTwoNumbers)}: {CheckSwitch(oneTwoNumbers)}");
+Console.WriteLine($"{nameof(oneTwoTenNumbers)}: {CheckSwitch(oneTwoTenNumbers)}");
+Console.WriteLine($"{nameof(oneTwoThreeTenNumbers)}: {CheckSwitch(oneTwoThreeTenNumbers)}");
+Console.WriteLine($"{nameof(primeNumbers)}: {CheckSwitch(primeNumbers)}");
+Console.WriteLine($"{nameof(fibonacciNumbers)}: {CheckSwitch(fibonacciNumbers)}");
+Console.WriteLine($"{nameof(emptyNumbers)}: {CheckSwitch(emptyNumbers)}");
+Console.WriteLine($"{nameof(threeNumbers)}: {CheckSwitch(threeNumbers)}");
+Console.WriteLine($"{nameof(sixNumbers)}: {CheckSwitch(sixNumbers)}");
+
+static string CheckSwitch(int[] values) => values switch
+{
+    [] => "Empty array",
+    [1, 2, _, 10] => "Contains 1, 2, any single number, 10.",
+    [1, 2, .., 10] => "Contains 1, 2, any range including empty, 10.",
+    [1, 2] => "Contains 1 then 2.",
+    [int item1, int item2, int item3] => $"Contains {item1} then {item2} then {item3}",
+    [0, _] => "Starts with 0, then one other number.",
+    [0, ..] => "Starts with 0, then any range of numbers.",
+    [2, .. int[] others] => $"Starts with 2, then {others.Length} more numbers.",
+    [..] => "Any items in any order.",
+};
+#endregion
---
+++ cs8dotnet12/Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,101 @@
+﻿using static System.Convert; // to use the ToInt32 method
+using static System.Console;
+using System.Globalization; // to use CultureInfo
+
+#region How negative numbers are represented in binary
+/*
+WriteLine("{0, 12} {1, 34}", "Decimal", "Binary");
+WriteLine("{0, 12} {0, 34:B32}", int.MaxValue); 
+
+for (int i = 8; i >= -8; i--)
+{
+    WriteLine("{0, 12} {0, 34:B32}", i);
+}
+WriteLine("{0, 12} {0, 34:B32}", int.MinValue);
+*/
+#endregion
+
+#region Rounding numbers
+/*
+double g = 9.8;
+int h = ToInt32(g); // a method of System.Convert
+WriteLine($"g is {g} and h is {h}.");
+
+double[,] doubles =
+{
+    { 9.49, 9.5, 9.51 },
+    { 10.49, 10.5, 10.51 },
+    { 11.49, 11.5, 11.51 },
+    { 12.49, 12.5, 12.51 },
+    { -9.49, -9.5, -9.51 },
+    { -10.49, -10.5, -10.51 },
+    { -11.49, -11.5, -11.51 },
+    { -12.49, -12.5, -12.51 },
+
+};
+
+WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7} ");
+    }
+    WriteLine("|");
+}
+WriteLine();
+
+foreach (double n in doubles)
+{
+    WriteLine(format:
+        "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}", n, Math.Round(n, 0, MidpointRounding.AwayFromZero));
+}*/
+#endregion
+
+#region Converting types to string
+/*
+int number = 12;
+bool boolean = true;
+DateTime now = DateTime.Now;
+object me = new();
+
+WriteLine(number.ToString());   // WriteLine implicitly converts args to string, so invoking ToString() is unnecessary
+WriteLine(boolean.ToString());
+WriteLine(now.ToString());
+WriteLine(me.ToString());
+*/
+#endregion
+
+#region Converting from a binary object to a string
+/*
+// allocate an array of 128 bytes
+byte[] binaryObject = new byte[128];
+
+// populate the array with random bytes.
+Random.Shared.NextBytes(binaryObject);
+
+WriteLine("Binary Object as bytes:");
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X2} ");
+}
+WriteLine(0);
+
+// convert the array to Base 64 string and output as text.
+string encoded = ToBase64String(binaryObject);
+WriteLine($"Binary Object as Base64: {encoded}");
+*/
+#endregion
+
+#region
+/*
+// set the current culture to make sure date parsing works
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+int friends = int.Parse("27");
+DateTime birthday = DateTime.Parse("4 June 1980");
+WriteLine($"I have {friends} friends to invite to my party.");
+WriteLine($"My birthday is {birthday}.");
+WriteLine($"My birthday is {birthday:D}.");
+*/
+#endregion
