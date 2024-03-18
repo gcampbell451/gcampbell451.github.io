83b146e21ef7b47281e127ff6e0ee4fd4c2f004d
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-03-18 17:29:04 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter04/Debugging/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {Add(a, b)}");
+WriteLine("Press Enter to end the app.");
+ReadLine(); // wait for user to press enter
+
+
+double Add(double a, double b)
+{
+    return a * b;  // deliberate bug
+}
