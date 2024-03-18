0218be9775e1fe3acf716959e435a6cf7fcd429a
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-03-18 17:44:30 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿using CallStackExceptionHandlingLib; // to use Processor
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+void Beta()
+{
+    WriteLine("In Beta");
+
+    try
+    {
+        Processor.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        throw;
+    }
+
+    
+}
