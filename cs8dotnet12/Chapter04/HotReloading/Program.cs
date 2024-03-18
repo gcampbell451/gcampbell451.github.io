813c64b7ccca184b72f395439be91cf73616dc78
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-03-18 17:30:01 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter04/HotReloading/Program.cs
@@ -0,0 +1,8 @@
+﻿// visual studio 2022: run the app, change the message, click Hot RelLoad.
+// visual studio code: run the app using dotnet watch, change the message.
+
+while (true)
+{
+    WriteLine("Hello, Hot Reload!");
+    await Task.Delay(2000);
+}
