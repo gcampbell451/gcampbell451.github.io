03dbbef5cb088ea9d29e1ba1c7ab762fc90458f3
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-04-03 14:33:57 -0400

    Add files via upload

--- cs8dotnet12/Chapter05/PeopleApp/Program.cs
+++ cs8dotnet12/Chapter05/PeopleApp/Program.cs
@@ -1,6 +1,9 @@
-﻿using Packt.Shared; // to use Person
+﻿using Microsoft.Win32.SafeHandles;
+using Packt.Shared; // to use Person
+using Fruit = (string Name, int Number); // aliasing a tuple type
 
-ConfigureConsole();  // sets current culture ot US English.
+// ConfigureConsole();  // sets current culture ot US English.
+
 
 Person bob = new();
 
@@ -13,7 +16,7 @@ bob.Born = new DateTimeOffset(
     hour: 16, minute: 28, second: 0,
     offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time
 
-WriteLine(format: "{0} was born on {1:D}.", bob.Name, bob.Born);
+// WriteLine(format: "{0} was born on {1:D}.", bob.Name, bob.Born);
 
 Person alice = new()
 {
@@ -21,7 +24,7 @@ Person alice = new()
     Born = new(1998, 3, 7, 16, 28, 0, TimeSpan.Zero)
 };
 
-WriteLine(format:"{0} was born on {1:d}.", alice.Name, alice.Born);
+// WriteLine(format:"{0} was born on {1:d}.", alice.Name, alice.Born);
 
 # endregion
 
@@ -29,11 +32,11 @@ WriteLine(format:"{0} was born on {1:d}.", alice.Name, alice.Born);
 
 bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
 
-WriteLine(format: "{0}'s favorite wonder is {1}. Its integer is {2}.", bob.Name, bob.FavoriteAncientWonder, (int)bob.FavoriteAncientWonder);
+// WriteLine(format: "{0}'s favorite wonder is {1}. Its integer is {2}.", bob.Name, bob.FavoriteAncientWonder, (int)bob.FavoriteAncientWonder);
 
 bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
 
-WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
+// WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
 
 # endregion
 
@@ -50,11 +53,11 @@ bob.Children.Add(new Person { Name = "Bella" });
 // works with C# 9 and later
 bob.Children.Add(new() { Name = "Zoe" });
 
-WriteLine($"{bob.Name} has {bob.Children.Count} children:");
-foreach (Person child in bob.Children)
+//WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+/*foreach (Person child in bob.Children)
 {
     WriteLine($"> {child.Name}");
-}
+}*/
 
 #endregion
 
@@ -65,25 +68,316 @@ BankAccount.InterestRate = 0.012M; // store a shared value in static field
 BankAccount jonesAccount = new();
 jonesAccount.AccountName = "Mrs. Jones";
 jonesAccount.Balance = 2400;
-WriteLine(format: "{0} earned {1:C} interest.", jonesAccount.AccountName, jonesAccount.Balance * BankAccount.InterestRate);
+// WriteLine(format: "{0} earned {1:C} interest.", jonesAccount.AccountName, jonesAccount.Balance * BankAccount.InterestRate);
 
 BankAccount gerrierAccount = new();
 gerrierAccount.AccountName = "Mrs. Gerrier";
 gerrierAccount.Balance = 98;
-WriteLine(format: "{0} earned {1:C} interest.", gerrierAccount.AccountName, gerrierAccount.Balance * BankAccount.InterestRate);
+// WriteLine(format: "{0} earned {1:C} interest.", gerrierAccount.AccountName, gerrierAccount.Balance * BankAccount.InterestRate);
 
 #endregion
 
 #region Making a field constant
 
 // constant fields are accessible via the type.
-WriteLine($"{bob.Name} is a {Person.Species}.");
+// WriteLine($"{bob.Name} is a {Person.Species}.");
 
 #endregion
 
 #region Making a field read-only
-
+/*
 // read-only fileds are accessible via the variable.
 WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
+*/
+#endregion
+
+#region Requiring fields to be set during instantiation
+/*
+Book book = new()
+{
+    Isbn = "978-1803237800",
+    Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+};
+ */
+
+Book book = new(isbn: "978-1803237800",
+    title: "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J. Price",
+    PageCount = 821
+};
+
+// WriteLine("{0}: {1} written by {2} has {3:N0} pages.", book.Isbn, book.Title, book.Author, book.PageCount);
+
+Person blankPerson = new();
+
+// WriteLine(format: 
+//    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.", 
+//    blankPerson.HomePlanet, 
+//    blankPerson.Instantiated);
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+
+// WriteLine(format:
+//    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+//    gunny.Name,
+//    gunny.HomePlanet,
+//    gunny.Instantiated);
 
 #endregion
+
+#region Working with methods and tuples
+
+// bob.WriteToConsole();
+// WriteLine(bob.GetOrigin());
+
+#endregion
+
+#region Defining and passing parameters to methods
+
+// WriteLine(bob.SayHello());
+// WriteLine(bob.SayHello("Emily"));
+
+#endregion
+
+#region Passing optional parameters, naming parameter values when calling methods, mixing optional and required parameters
+
+// WriteLine(bob.OptionalParameters(3));
+// WriteLine(bob.OptionalParameters(3, "Jump!", 98.5));
+// WriteLine(bob.OptionalParameters(3, number: 52.7, command: "Hide!"));
+// WriteLine(bob.OptionalParameters(3, "Poke!", active: false));
+
+#endregion
+
+#region Controlling how parameters are passed
+/*
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 40;
+
+WriteLine($"Before: a={a}, b={b}, c={c}, d={d}");
+
+bob.PassingParameters(a, b, ref c, out d);
+
+WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
+
+int e = 50;
+int f = 60;
+int g = 70;
+WriteLine($"Before: e={e}, f={f}, g={g}, h doesn't exist yet!");
+
+// simplified C# 7 or later syntax for the out parameter
+bob.PassingParameters(e, f, ref g, out int h);
+WriteLine($"After: e={e}, f={f}, g={g}, h={h}");
+*/
+#endregion
+
+#region Combining multiple returned values using tuples, naming the fields of a tuple
+/*
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children");
+
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children.");
+*/
+#endregion
+
+#region Aliasing and deconstructing tuples, deconstructing other types using tuples
+/*
+// without an aliased tuple type.
+// var fruitNamed = bob.GetNamedFruit();
+
+// with an aliased tuple type.
+Fruit fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+// store return value in a tuple variable with two named fields
+(string name, int number) namedFields = bob.GetNamedFruit();
+
+// you can then access the named fileds
+WriteLine($"{namedFields.name}, {namedFields.number}");
+
+// deconstruct the return value into two separate variables
+(string name, int number) = bob.GetNamedFruit();
+
+// you can then access the separate variables
+WriteLine($"{name}, {number}");
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed tuple: {fruitName}, {fruitNumber}");
+
+
+var (name1, dob1) = bob; // implicitly calls the Deconstruct method.
+WriteLine($"Deconstructed person: {name1}, {dob1}");
+
+var (name2, dob2, fav2) = bob;
+WriteLine($"Deconstructed person: {name1}, {dob1}, {fav2}");
+*/
+#endregion
+
+#region Implementing functionality using local functions
+/*
+// change to -1 to make the exception handling code execute
+int number = -1;
+
+try
+{
+    WriteLine($"{number}! is {Person.Factorial(number)}");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says: {ex.Message} number was {number}.");
+}
+*/
+#endregion
+
+#region Defining read-only properties
+/*
+Person sam = new()
+{
+    Name = "Sam",
+    Born = new(1969, 6, 25, 0, 0, 0, TimeSpan.Zero)
+};
+/*
+WriteLine(sam.Origin);
+WriteLine(sam.Greeting);
+WriteLine(sam.Age);
+*/
+#endregion
+
+#region Defining settable properties
+/*
+sam.FavoriteIceCream = "Chocolate Fudge";
+WriteLine($"Sam's favorite ice cream flavor is {sam.FavoriteIceCream}.");
+
+string color = "red";
+
+try
+{
+    sam.FavoritePrimaryColor = color;
+    WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
+}
+catch (Exception ex)
+{
+    WriteLine("Tried to  set {0} to '{1}': {2}", nameof(sam.FavoritePrimaryColor), color, ex.Message);
+}
+*/
+#endregion
+
+#region Defining Indexers
+/*
+sam.Children.Add(new() { Name = "Charlie", Born = new(2010, 3, 18, 0, 0, 0, TimeSpan.Zero )});
+sam.Children.Add(new() { Name = "Ella", Born = new(2020, 12, 24, 0, 0, 0, TimeSpan.Zero) });
+
+// get using Children list
+WriteLine($"Sam's first child is {sam.Children[0].Name}.");
+WriteLine($"Sam's second child is {sam.Children[1].Name}.");
+
+// get using the int indexer
+WriteLine($"Sam's first child is {sam[0].Name}.");
+WriteLine($"Sam's second child is {sam[1].Name}.");
+
+// get using the string indexer
+WriteLine($"Sam's child named Ella is {sam["Ella"].Age} years old.");
+*/
+
+
+#endregion
+
+#region Pattern matching with objects
+
+// an array containing a mix of passenter types
+Passenger[] passengers =
+{
+    new FirstClassPassenger { AirMiles = 1_419, Name = "Suman"},
+    new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy"},
+    new BusinessClassPassenger { Name = "Janice"},
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave"},
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit"},
+};
+
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        /* C# 8 syntax
+        FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
+        FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
+        FirstClassPassenger _ => 2_000M, */
+
+        // C# 9 or later syntax
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35_000 => 1_500M,
+            > 15_000 => 1_750M,
+            _ => 2_000M
+        },
+        BusinessClassPassenger _ => 1_000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    }; ;
+    // WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
+
+#endregion
+
+#region Working with, defining, equality of record types, positional data members in records
+
+ // working with init-only properties
+
+ImmutablePerson jeff = new()
+{
+    FirstName = "Jeff",
+    LastName = "Winger"
+};
+
+// jeff.FirstName = "Geoff";
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+ImmutableVehicle repaintedCar = car
+    with { Color = "Polymetal Grey Metallic" };
+//WriteLine($"Original car color was {car.Color}.");
+//WriteLine($"New car color is {repaintedCar.Color}.");
+
+AnimalClass ac1 = new() { Name = "Rex" };
+AnimalClass ac2 = new() { Name = "Rex" };
+
+// WriteLine($"ac1 == ac2: {ac1 == ac2}");
+
+AnimalRecord ar1 = new() { Name = "Rex" };
+AnimalRecord ar2 = new() { Name = "Rex" };
+
+// WriteLine($"ar1 == ar2: {ar1 == ar2}");
+
+ImmutableAnimal oscar = new("Oscar", "Labrador");
+var (who, what) = oscar; // calls the deconstruct method
+// WriteLine($"{who} is a {what}.");
+
+#endregion
+
+#region Defining a primary constructor for a class
+
+Headset vp = new("Apple", "Vision Pro");
+WriteLine($"{vp.ProductName} is made by {vp.Manufacturer}.");
+
+Headset holo = new();
+WriteLine($"{holo.ProductName} is made by {holo.Manufacturer}.");
+
+Headset mq = new() { Manufacturer = "Meta", ProductName = "Quest 3" };
+WriteLine($"{mq.ProductName} is made by {mq.Manufacturer}.");   
+
+#endregion
+
