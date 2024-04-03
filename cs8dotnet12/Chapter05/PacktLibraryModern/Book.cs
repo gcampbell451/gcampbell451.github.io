92ffb282acac3c9c4dbb6eb288b7f95c61cb16f5
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-04-03 14:32:35 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter05/PacktLibraryModern/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis; // to use [SetsRequiredMembers]
+namespace Packt.Shared;
+
+public class Book
+{
+    // needs .NET 7 or later as well as C# 11 or later
+    public required string? Isbn;
+    public required string? Title;
+
+    // works with any version of .NET
+    public string? Author;
+    public int PageCount;
+
+    // constructor for use with object initializer syntax
+    public Book() { }
+
+    // constructor with parameters to set required fields
+    [SetsRequiredMembers]
+    public Book(string? isbn, string? title)
+    {
+        Isbn = isbn;
+        Title = title;  
+    }
+}
---
+++ cs8dotnet12/Chapter05/PacktLibraryModern/Records.cs
@@ -0,0 +1,17 @@
+﻿namespace Packt.Shared;
+
+public class ImmutablePerson
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init;}
+}
+
+public record ImmutableVehicle
+{
+    public int Wheels { get; init; }
+    public string? Color { get; init; }
+    public string? Brand { get; init; }
+}
+
+// define a record that auto-generates the props, constructor, and deconstructor
+public record ImmutableAnimal(string Name, string Species);
