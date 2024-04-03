10e459f7803fcf1316bff734c07701304b1450f0
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-04-03 14:33:15 -0400

    Add files via upload

--- cs8dotnet12/Chapter05/PacktLibraryNetStandard2/Person.cs
+++ cs8dotnet12/Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -1,13 +1,16 @@
 ﻿// ALL types in this file will be defined in this file-scoped namespace
 namespace Packt.Shared;
 
-public class Person : object
+public partial class Person : object
 {
     #region Fields: Data or state for this person.
 
     public string? Name; // ? means it can be null.
     public DateTimeOffset Born;
-    public WondersOfTheAncientWorld FavoriteAncientWonder;
+    
+    // this has been moved to PersonAutoGen.cs as a property
+    // public WondersOfTheAncientWorld FavoriteAncientWonder;
+
     public WondersOfTheAncientWorld BucketList;
     public List<Person> Children = new();
 
@@ -16,7 +19,117 @@ public class Person : object
 
     // read-only fields, can be set at runtime
     public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
+
 
     #endregion
 
+    #region Initializing fields with constructors
+
+    public Person()
+    {
+        // constructors can set default values for fields
+        // including any read-only fileds like Instantiated
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+
+    #endregion
+
+    #region Methods: Actions the type can perform
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+
+    public string GetOrigin()
+    {
+        return $"{Name} was born on {HomePlanet}.";
+    }
+
+    public string SayHello()
+    {
+        return $"{Name} says 'Hello!'";
+    }
+
+    public string SayHello(string name)
+    {
+        return $"{Name} says 'Hello, {name}!'";
+    }
+
+    public string OptionalParameters(int count, string command = "Run!", double number = 0.0, bool active = true)
+    {
+        return string.Format(
+            "command is {0}, number is {1}, active is {2}", command, number, active );
+    }
+  
+    public void PassingParameters(int w, in int x, ref int y, out int z)
+    {
+        // out parameters cannot have a default and they must be initialized inside the method
+        z = 100;
+
+        // increment each parameter except the read-only x.
+        w++;
+        // x++; // gives a compiler error
+        y++;
+        z++;
+
+        WriteLine($"In the method: w={w}, x={x}, y={y}, z={z}");
+    }
+
+    // method that returns a tuple: (string, int)
+    public (string, int) GetFruit()
+    {
+        return ("Apples", 5);
+    }
+
+    // method that returns a tuple with named fields
+    public (string Name, int Number) GetNamedFruit()
+    {
+        return ("Apples", 5);
+    }
+
+    // deconstructors: break down this object into parts, can have as many as want as long as they have diff signatures
+    public void Deconstruct(out string? name, out DateTimeOffset dob)
+    {
+        name = Name;
+        dob = Born;
+    }
+
+    public void Deconstruct(out string? name, out DateTimeOffset dob, out WondersOfTheAncientWorld fav)
+    {
+        name = Name;
+        dob = Born;
+        fav = FavoriteAncientWonder;
+    }
+
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+
+        int localFactorial(int localNumber) // local function
+        {
+            if (localNumber == 0) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+
+ 
+
+    #endregion
 }
+
+
+
---
+++ cs8dotnet12/Chapter05/PacktLibraryNetStandard2/PersonAutoGen.cs
@@ -0,0 +1,106 @@
+﻿namespace Packt.Shared;
+
+// this file simulates an auto-generated class
+public partial class Person
+{
+    #region Properties: Methods to get and/or set data or state
+
+    // a readonly property defined using C# 1 to 5 syntax
+    public string Origin
+    {
+        get
+        {
+            return string.Format("{0} was born on {1}.", Name, HomePlanet);
+        }
+    }
+
+    // two readonly properties defined using C# 6 or later lambda expression syntax
+    public string Greeting => $"{Name} says 'Hello!'";
+    public int Age => DateTime.Today.Year - Born.Year;
+
+    // a read-write property defined using C# 3 auto-syntax
+    public string? FavoriteIceCream {  get; set; }
+
+    // a private backing field to store the property value
+    private string? _favoritePrimaryColor;
+
+    // a public property to read and write to the field
+    public string? FavoritePrimaryColor
+    {
+        get
+        {
+            return _favoritePrimaryColor;
+        }
+        set
+        {
+            switch (value?.ToLower())
+            {
+                case "red":
+                case "green":
+                case "blue":
+                    _favoritePrimaryColor = value;
+                    break;
+                default:
+                    throw new ArgumentException(
+                        $"{value} is not a primary color. " + "Choose from: red, green, blue.");
+            }
+        }
+    }
+
+    private WondersOfTheAncientWorld _favoriteAncientWonder;
+
+    public WondersOfTheAncientWorld FavoriteAncientWonder
+    {
+        get { return _favoriteAncientWonder; }
+        set
+        {
+            string wonderName = value.ToString();
+            
+            if (wonderName.Contains(','))
+            {
+                throw new ArgumentException(
+                    "Favorite ancient wonder can only have a single enum value.", 
+                    nameof(FavoriteAncientWonder));
+            }
+            if (!Enum.IsDefined(typeof(WondersOfTheAncientWorld), value))
+            {
+                throw new ArgumentException(
+                    $"{value} is not a member of the WondersOfheAncientWorld enum.",
+                    nameof(FavoriteAncientWonder));
+            }
+            _favoriteAncientWonder = value;
+        }
+    }
+
+
+    #endregion
+
+    #region Indexers: Preoerties that use array syntax to access them
+
+    public Person this[int index]
+    {
+        get
+        {
+            return Children[index]; // pass on to the List<T> indexer
+        }
+        set 
+        { 
+            Children[index] = value;
+        }
+    }
+
+    public Person this[string name]
+    {
+        get
+        {
+            return Children.Find(p => p.Name == name);
+        }
+    }
+
+
+
+    #endregion
+
+
+}
+
