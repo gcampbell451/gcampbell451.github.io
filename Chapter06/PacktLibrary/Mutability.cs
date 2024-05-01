87e3c78b42e25c5cad65e178bb933aeb1b3bfadb
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-01 15:31:09 -0400

    Add files via upload

---
+++ Chapter06/PacktLibrary/Mutability.cs
@@ -0,0 +1,23 @@
+﻿namespace Packt.Shared;
+
+// a mutable record class
+public record class C1
+{
+    public string? Name { get; set; }
+}
+
+// an immutable record class
+public record class C2(string? Name);
+
+// a mutable record struct
+public record struct S1
+{
+    public string? Name { get; set; }
+}
+
+// another mutable record struct
+public record struct S2(string? Name);
+
+// an immutable record struct
+
+public readonly record struct S3(string? Name);
