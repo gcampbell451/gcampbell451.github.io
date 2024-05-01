905894570485af903ec532a34f2a1607ccf8577d
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-01 15:33:20 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter06/PacktLibrary/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions; // to use regex
+
+namespace Packt.Shared;
+
+public static class StringExtensions
+{
+    public static bool IsValidEmail(this string input)
+    {
+        // use a simple regex to check that the input string is a valid email
+
+        return Regex.IsMatch(input,
+            @"[a-zA-Z0-9\.-_]+@[a-zA-Z0-9\._]+");
+    }
+}
