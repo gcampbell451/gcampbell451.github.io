3a3eeef54a49ebb515950b4588334dbb1e6ef7a7
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-20 16:22:04 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter07/AotConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization; // to use CultureInfo
+using System.Reflection; // to use AssemblyName
+using System.Reflection.Emit; // to use AssemblyBuilder
+
+WriteLine("This is an ahead-of-time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
+WriteLine("OS version : {0}", Environment.OSVersion);
+
+Write("Press any key to exit.");
+ReadKey(intercept: true); // do not output the key that was pressed
+
+// AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly(
+//    new AssemblyName("MyAssembly"), AssemblyBuilderAccess.Run);
