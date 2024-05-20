8d55215467cd5f450e8c86160946a994f01ffb3e
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-20 17:41:24 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter09/ControllingJson/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization; // to use [JsonInclude]
+
+namespace Packt.Shared;
+
+public class Book
+{
+    // constructor to set non-nullable property
+    public Book(string title)
+    {
+        Title = title;
+    }
+
+    // properties
+    public string Title { get; set; }
+    public string? Author { get; set; }
+
+    // fields
+    [JsonInclude] // include this field
+    public DateTime PublishDate;
+    [JsonInclude]
+    public DateTimeOffset Created;
+
+    public ushort Pages;
+}
---
+++ cs8dotnet12/Chapter09/ControllingJson/Program.cs
@@ -0,0 +1,35 @@
+﻿using Packt.Shared; // to use Book
+using System.Security.Cryptography;
+using System.Text.Json; // to use JsonSerializer
+
+Book csharpBook = new(title:
+    "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J Price",
+    PublishDate = new(2023, 11, 14),
+    Pages = 823,
+    Created = DateTimeOffset.UtcNow,
+};
+
+JsonSerializerOptions options = new()
+{
+    IncludeFields = true, // includes all fields
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+
+string path = Combine(CurrentDirectory, "book.json");
+
+using (Stream fileStream = File.Create(path))
+{
+    JsonSerializer.Serialize(utf8Json: fileStream, value: csharpBook, options);
+}
+
+WriteLine("**** File Info ****");
+WriteLine($"File: {GetFileName(path)}");
+WriteLine($"Path: {GetDirectoryName(path)}");
+WriteLine($"Size {new FileInfo(path).Length:N0} DeriveBytes");
+WriteLine("/-------------------");
+WriteLine(File.ReadAllText(path));
+WriteLine("------------------/");
