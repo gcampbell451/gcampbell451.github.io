83db86b867887c31c9afc638927f2850b819649b
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-20 16:10:35 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using Packt.Shared;
+
+using System.Xml.Linq; // to use XDocument
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid color value? {1}", 
+    hex, hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid SML element? {1}",
+    xmlTag, xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine();
+WriteLine("Is {0} a valid password? {1}",
+    password, password.IsValidPassword());
