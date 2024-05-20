31920c33ecfb411a404f98ebbef0df71375ec524
gcampbell451 <65924401+gcampbell451@users.noreply.github.com>
2024-05-20 16:49:08 -0400

    Add files via upload

---
+++ cs8dotnet12/Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+
+
+Write("Enter your age: ");
+string input = ReadLine()!;
+Regex ageChecker = DigitsOnly();
+WriteLine(ageChecker.IsMatch(input) ? "Thank you!" : $"This is not a valid age: {input}");
+
+
+string films = """
+    "Monsters, Inc.", "I, Tonya", "Lock, Stock, and Two Smoking Barrels"
+    """;
+
+WriteLine($"Films to split: {films}");
+
+string[] filmsDumb = films.Split(',');
+
+WriteLine("Splitting with string.Split method: ");
+foreach (string film in filmsDumb)
+{
+    WriteLine($"  {film}");
+}
+Regex csv = CommaSeparator();
+
+MatchCollection filmsSmart = csv.Matches(films);
+
+WriteLine("Splitting with regular expression:");
+foreach (Match film in filmsSmart)
+{
+    WriteLine($"  {film.Groups[2].Value}");
+}
